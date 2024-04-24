1982702e7659794b165363a894858e6955c920a2
R3messa1 <51412906+R3messa1@users.noreply.github.com>
2024-04-24 17:06:13 +0300

    Add files via upload

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IDamageable
+{
+
+    private float currentSpeed = 3;
+
+    private Rigidbody2D body;
+
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate(){
+
+        if(!GameManager.Instance.IsGamePlay()){
+            return;
+        }
+
+        GetPlayer();
+
+        Move();
+    }
+
+    private void Move(){
+
+        if(playerTransform == null){
+            return;
+        }
+
+        Vector2 direction = (playerTransform.position - transform.position).normalized;
+
+        body.MovePosition(body.position + direction * currentSpeed * Time.fixedDeltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GetPlayer(){
+
+        if(playerTransform == null){
+
+            playerTransform = GameManager.Instance.playerController.transform;
+        }
+    }
+
+    public void TakeDamage(int damage){
+
+
+    }
+
+    public void Die(){
+        
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+
+    public float spawnRadius = 10f;
+
+    public float spawnInterval = 2f;
+    private Transform playerTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
